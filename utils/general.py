ecc42c76c55cea7756e4956e49e0670babd5f3dc
Shravan Murali <shravanmurali@gmail.com>
2018-02-14 17:17:21 +0530

    Add general SMTP mailing util

--- utils/general.py
+++ utils/general.py
@@ -57,6 +57,40 @@ def phno_validate(phno):
         return False
     return True
 
+def send_mail(html, subject, email):
+    me = "synergy18.web@outlook.com"
+    you = email
+
+    # Create message container - the correct MIME type is multipart/alternative.
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = me
+    msg['To'] = you
+
+    # Record the MIME types of both parts - text/plain and text/html.
+    part1 = MIMEText(html, 'html')
+
+    # Attach parts into message container.
+    # According to RFC 2046, the last part of a multipart message, in this case
+    # the HTML message, is best and preferred.
+    msg.attach(part1)
+
+    # Send the message via local SMTP server.
+    s = smtplib.SMTP('smtp-mail.outlook.com', 587)
+    s.starttls()
+
+    creds_file = open(settings.BASE_DIR+"/utils/mail_creds", "r+")
+    creds = []
+    for k in creds_file:
+        creds.append(k.strip())
+
+    s.login(creds[0], creds[1])
+    # sendmail function takes 3 arguments: sender's address, recipient's address
+    # and message to send - here it is sent as one string.
+    s.sendmail(me, you, msg.as_string())
+    s.quit()
+
+
 def send_welcome_mail(fullname, email, uniq):
     me = "synergy18.web@outlook.com"
     you = email
