6c25fa80153afb483a9b9fd4b168e1efa60b658e
Anirudh Sundar <anirudhsundar@hotmail.com>
2019-01-29 19:06:17 +0530

    Fix resize eventlistener for particles-js

--- templates/static/js/start-particles.js
+++ templates/static/js/start-particles.js
@@ -3,11 +3,9 @@ particlesJS.load('particles-js', '/static/configs/particles.json', function () {
 });
 
 window.addEventListener('resize', () => {
-    document.querySelector("#particles-js").width = `${window.innerWidth}px`;
-    document.querySelector("#particles-js").height = `${document.querySelector("#wrapper").offsetHeight - 10}px)`;
+    document.querySelector("#particles-js").style.width = `${window.innerWidth}px`;
+    document.querySelector("#particles-js").style.height = `${document.querySelector("#wrapper").offsetHeight - 10}px`;
 });
 
 document.querySelector("#particles-js").style.width = `${window.innerWidth}px`;
 document.querySelector("#particles-js").style.height = `${document.querySelector("#wrapper").offsetHeight - 10}px`;
-console.log(document.querySelector("#particles-js").style.width, document.querySelector("#particles-js").style.height)
-console.log(document.querySelector("#wrapper").offsetHeight - 10)
