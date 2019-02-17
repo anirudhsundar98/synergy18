0a389727869d28f58c79a1a5b9b37dddb3154716
Anirudh Sundar <anirudhsundar@hotmail.com>
2019-02-17 14:10:06 +0530

    Fix particles js div positioning, Remove JS based resizing logic

--- templates/static/js/start-particles.js
+++ templates/static/js/start-particles.js
@@ -2,10 +2,19 @@ particlesJS.load('particles-js', '/static/configs/particles.json', function () {
     console.log('callback - particles.js config loaded');
 });
 
-window.addEventListener('resize', () => {
-    document.querySelector("#particles-js").style.width = `${window.innerWidth}px`;
-    document.querySelector("#particles-js").style.height = `${window.innerHeight - 10}px`;
-});
+// window.addEventListener('resize', () => {
+//     resizeParticlesCanvas();
+// });
 
-document.querySelector("#particles-js").style.width = `${window.innerWidth}px`;
-document.querySelector("#particles-js").style.height = `${window.innerHeight - 10}px`;
+// function resizeParticlesCanvas() {
+//     if (document.querySelector("#particles-js")) {
+//         document.querySelector("#particles-js").style.width = `${window.innerWidth}px`;
+//         if (document.querySelector("#wrapper").offsetHeight > window.innerHeight) {
+//             document.querySelector("#particles-js").style.height = `${document.querySelector("#wrapper").offsetHeight - 10}px`;
+//         } else {
+//             document.querySelector("#particles-js").style.height = `${window.innerHeight - 10}px`;
+//         }
+//     }
+// }
+
+// resizeParticlesCanvas();
